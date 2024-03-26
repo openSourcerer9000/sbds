61d2db595c7b629770e9dd21195c56acb5311f67
openSourcerer <seanrm100@gmail.com>
2024-03-26 15:25:38 -0500

    doc

--- GetBuildingsExample.py
+++ GetBuildingsExample.py
@@ -30,3 +30,25 @@ if __name__=='__main__':
         YOLOcheckpoint=ckpt,
         overwrite=False,
         )
+
+    # Extracts building footprints from satellite imagery using a custom fine-tuned model
+    # for automatic segmentation. It supports downloading new imagery for a specified area of
+    # interest (AOI) or processing an existing satellite image. The extracted building footprints
+    # are saved as vector data in the GeoPackage format.
+
+    # Parameters:
+    # - image: Specifies the path to a satellite image or 'download' to download imagery
+    #   for the AOI defined in `extents`.
+    # - outVector: Path where the extracted building footprints vector file will be saved.
+    #   If 'default', a path is generated based on the input image's location and name.
+    # - box_threshold: Threshold for the bounding box detection in the segmentation model.
+    # - extents: Path to a file defining the AOI for downloading new imagery. Required if
+    #   `image` is 'download'.
+    # - source: Defines the source of satellite imagery for downloading when `image` is 'download'.
+    # - overwrite: If True, allows overwriting existing files when downloading or saving output files.
+    # - YOLOcheckpoint: Specifies the path to the checkpoint file of the YOLO model used for
+    #   building detection.
+
+    # The function processes the satellite image, applying a segmentation model to detect buildings
+    # and saving the results as vector data. It supports handling large images in patches and 
+    # refines the output to include only buildings within the AOI if an `extents` file is provided.
